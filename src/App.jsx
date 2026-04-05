a9b50b60a5296f04e4e458221755150a79cf1662
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-06 02:49:13 +0300

    First App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import MoviewList from "./components/MovieList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MoviewList />
+    </>
+  );
+}
+
+export default App;
